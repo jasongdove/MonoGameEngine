d3ff893ce973df65b71beb35b64ee5c666f2bbaf
Jason Dove <jgdove@gmail.com>
2013-04-09 20:44:46 -0500

    filled out menu screen

---
+++ src/GameEngine/MenuEntry.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameEngine
+{
+    public abstract class MenuEntry
+    {
+        private EntryState _state = EntryState.Normal;
+        private Dictionary<bool, Rectangle> _displayRegion;
+        private bool _hasSubMenu = false;
+        private Color _color = Color.White;
+        private float _scale = 1.0f;
+        private float _opacity = 1.0f;
+
+        protected MenuEntry()
+        {
+            BoundingRectangle = new Rectangle(0, 0, 0, 0);
+        }
+
+        protected MenuEntry(MenuScreen screen, string title)
+        {
+            Color = screen.Normal;
+            ParentMenu = screen;
+            EntryTitle = title;
+            BoundingRectangle = new Rectangle(0, 0, 0, 0);
+        }
+
+        protected MenuEntry(MenuScreen screen, Texture2D texture)
+        {
+            Color = screen.Normal;
+            ParentMenu = screen;
+            EntryTexture = texture;
+            BoundingRectangle = new Rectangle(0, 0, 0, 0);
+        }
+
+        protected MenuEntry(MenuScreen screen, string title, Texture2D texture)
+        {
+            Color = screen.Normal;
+            ParentMenu = screen;
+            EntryTitle = title;
+            EntryTexture = texture;
+            BoundingRectangle = new Rectangle(0, 0, 0, 0);
+        }
+
+        public event EventHandler OnHighlight;
+        public event EventHandler OnNormal;
+        public event EventHandler Selected;
+
+        public EntryState State
+        {
+            get { return _state; }
+        }
+
+        public string EntryTitle { get; set; }
+
+        public string EntryDescription { get; set; }
+
+        public Point EntryPadding { get; private set; }
+
+        public Texture2D EntryTexture { get; private set; }
+
+        public Rectangle BoundingRectangle { get; private set; }
+
+        public Point NumberOfEntries { get; set; }
+
+        public bool IsSheet
+        {
+            get { return NumberOfEntries.X == 1 && NumberOfEntries.Y == 1; }
+        }
+
+        public Vector2 DefaultPosition { get; private set; }
+
+        public Vector2 Position { get; set; }
+
+        public Vector2 Velocity { get; set; }
+
+        public Vector2 Acceleration { get; set; }
+
+        public MenuScreen ParentMenu { get; set; }
+
+        public MenuScreen SubMenu { get; private set; }
+
+        public bool HasSubMenu
+        {
+            get { return _hasSubMenu; }
+        }
+
+        public Color Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public float Scale
+        {
+            get { return _scale; }
+            set { _scale = value; }
+        }
+
+        public float Opacity
+        {
+            get { return _opacity; }
+            set { _opacity = value; }
+        }
+
+        public void UpdateEntry(GameTime gameTime)
+        {
+            Update(gameTime);
+
+            SpriteFont spriteFont = ParentMenu.SpriteFont;
+            Vector2 measure = spriteFont == null ? Vector2.Zero : spriteFont.MeasureString(EntryTitle);
+            var width = (int)(EntryTexture == null ? measure.X : EntryTexture.Width);
+            var height = (int)(EntryTexture == null ? measure.Y : EntryTexture.Height);
+            BoundingRectangle = new Rectangle((int)Position.X, (int)Position.Y, width, height);
+        }
+
+        public void AddSubMenu(MenuScreen subMenu)
+        {
+            _hasSubMenu = true;
+            SubMenu = subMenu;
+        }
+
+        public void SetRelativePosition(Vector2 relativePosition, MenuEntry entry, bool defaultPosition)
+        {
+            var position = new Vector2(entry.Position.X, entry.Position.Y);
+            if (entry.EntryTexture != null)
+            {
+                position.Y += entry.EntryTexture.Height;
+            }
+
+            SetPosition(Vector2.Add(position, relativePosition), defaultPosition);
+        }
+
+        public void SetRelativePosition(Vector2 relativeDefaultPosition, Vector2 relativeCurrentPosition, MenuEntry entry)
+        {
+            var defaultPosition = new Vector2(entry.DefaultPosition.X, entry.DefaultPosition.Y);
+            var currentPosition = new Vector2(entry.Position.X, entry.Position.Y);
+            if (entry.EntryTexture != null)
+            {
+                defaultPosition.Y += entry.EntryTexture.Height;
+                currentPosition.Y += entry.EntryTexture.Height;
+            }
+
+            SetPosition(Vector2.Add(defaultPosition, relativeDefaultPosition), Vector2.Add(currentPosition, relativeCurrentPosition));
+        }
+
+        public void SetPosition(Vector2 position, bool defaultPosition)
+        {
+            if (defaultPosition)
+            {
+                DefaultPosition = position;
+            }
+
+            Position = position;
+        }
+
+        public void SetPosition(Vector2 defaultPosition, Vector2 current)
+        {
+            DefaultPosition = defaultPosition;
+            Position = current;
+        }
+
+        public void AddTexture(Texture2D texture)
+        {
+            EntryTexture = texture;
+            NumberOfEntries = new Point(1, 1);
+        }
+
+        public void AddTexture(Texture2D texture, int numX, int numY, Rectangle selectedEntry, Rectangle normalEntry)
+        {
+            EntryTexture = texture;
+            NumberOfEntries = new Point(numX, numY);
+            _displayRegion = new Dictionary<bool, Rectangle>();
+            _displayRegion.Add(true, selectedEntry);
+            _displayRegion.Add(false, normalEntry);
+        }
+
+        public void AddPadding(int all)
+        {
+            EntryPadding = new Point(all, all);
+        }
+
+        public void AddPadding(int left, int top)
+        {
+            EntryPadding = new Point(left, top);
+        }
+
+        public virtual void Highlight()
+        {
+            Color = ParentMenu.Highlighted;
+            _state = EntryState.Highlight;
+            var handler = OnHighlight;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+
+        public virtual void Normal()
+        {
+            Color = ParentMenu.Normal;
+            _state = EntryState.Normal;
+            var handler = OnNormal;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+
+        public virtual void Select()
+        {
+            if (SubMenu != null)
+            {
+                _state = EntryState.Selected;
+                Color = ParentMenu.Selected;
+                SubMenu.ActivateScreen();
+                ParentMenu.ScreenManager.AddScreen(SubMenu);
+                ParentMenu.FreezeScreen();
+                ParentMenu.HideMouse();
+                SubMenu.EnableMouse(ParentMenu.MouseTexture);
+            }
+
+            var handler = Selected;
+            if (handler != null)
+            {
+                _state = EntryState.Selected;
+                Color = ParentMenu.Selected;
+                Selected(this, EventArgs.Empty);
+            }
+        }
+
+        public virtual void Draw(GameTime gameTime, bool isSelected)
+        {
+            SpriteBatch spriteBatch = ParentMenu.ScreenManager.SpriteBatch;
+            SpriteFont spriteFont = ParentMenu.SpriteFont;
+
+            var entryPosition = new Vector2(Position.X, Position.Y);
+
+            if (EntryTexture != null)
+            {
+                if (IsSheet)
+                {
+                    spriteBatch.Draw(EntryTexture, Position, Color.White * _opacity);
+                }
+                else if (_displayRegion != null)
+                {
+                    spriteBatch.Draw(EntryTexture, Position, _displayRegion[isSelected], Color.White * _opacity);
+                }
+
+                if (spriteFont != null && EntryTitle.Length > 0)
+                {
+                    Vector2 textDims = spriteFont.MeasureString(EntryTitle);
+
+                    float x = EntryPadding.X == 0 ? (EntryTexture.Width / 2) - (textDims.X / 2) : EntryPadding.X;
+                    float y = EntryPadding.Y == 0 ? (EntryTexture.Height / 2) - (textDims.Y / 2) : EntryPadding.Y;
+
+                    entryPosition += new Vector2(x, y);
+
+                    spriteBatch.DrawString(spriteFont, EntryTitle, entryPosition, _color * _opacity);
+                }
+            }
+            else if (spriteFont != null && EntryTitle.Length > 0)
+            {
+                spriteBatch.DrawString(spriteFont, EntryTitle, entryPosition, _color * _opacity, 0, Vector2.Zero, _scale, SpriteEffects.None, 0.0f);
+            }
+        }
+
+        public virtual void DrawDescription(GameTime gameTime, Rectangle boxPosition, Point padding, Color textColor, float scale)
+        {
+            SpriteBatch spriteBatch = ParentMenu.ScreenManager.SpriteBatch;
+            SpriteFont spriteFont = ParentMenu.SpriteFont;
+
+            if (spriteFont != null)
+            {
+                Vector2 textDims = spriteFont.MeasureString(EntryDescription) * _scale;
+
+                float x = padding.X;
+                float y = padding.Y;
+
+                var descriptionPosition = new Vector2(boxPosition.X + x, boxPosition.Y + y);
+                spriteBatch.DrawString(spriteFont, EntryDescription, descriptionPosition, textColor, 0.0f, Vector2.Zero, _scale, SpriteEffects.None, 1.0f);
+            }
+        }
+
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void AnimateHighlighted(GameTime gameTime);
+
+        public enum EntryState
+        {
+            Normal,
+            Highlight,
+            Selected
+        }
+    }
+}
--- src/GameEngine/MenuScreen.cs
+++ src/GameEngine/MenuScreen.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -6,6 +7,18 @@ namespace GameEngine
 {
     public abstract class MenuScreen : GameScreen
     {
+        private readonly List<MenuEntry> _menuEntries = new List<MenuEntry>();
+        private bool _mouseVisible = false;
+        private float _titleOpacity = 1.0f;
+        private Color _highlighted = Color.White;
+        private Color _selected = Color.White;
+        private Color _normal = Color.White;
+        private Texture2D _descriptionTexture;
+        private Color _descriptionColor;
+        private Color _descriptionBoxColor;
+        private float _descriptionScale;
+        private int _selectedEntry = 0;
+
         protected MenuScreen()
         {
             Removing += MenuScreen_Removing;
@@ -14,13 +27,103 @@ namespace GameEngine
         public event EventHandler Cancel;
 
         public GameScreen Parent { get; set; }
+
+        public SpriteFont SpriteFont { get; set; }
+
         public Texture2D BackgroundTexture { get; set; }
+
         public Vector2 BackgroundPosition { get; set; }
+
+        public Texture2D TitleTexture { get; set; }
+
+        public Vector2 InitialTitlePosition { get; set; }
+
+        public Vector2 TitlePosition { get; set; }
+
+        public Texture2D MouseTexture { get; set; }
+
+        public Rectangle MouseBounds { get; private set; }
+
+        public float TitleOpacity
+        {
+            get { return _titleOpacity; }
+            set { _titleOpacity = value; }
+        }
+
+        public Vector2 Position { get; set; }
+
+        public Color Highlighted
+        {
+            get { return _highlighted; }
+            set { _highlighted = value; }
+        }
+
+        public Color Selected
+        {
+            get { return _selected; }
+            set { _selected = value; }
+        }
+
+        public Color Normal
+        {
+            get { return _normal; }
+            set { _normal = value; }
+        }
+
+        public Rectangle DescriptionPosition { get; private set; }
+
+        public Point DescriptionPadding { get; set; }
+
         public MenuState MenuState { get; private set; }
+
+        public List<MenuEntry> MenuEntries
+        {
+            get { return _menuEntries; }
+        }
 
         public override bool AcceptsInput
         {
             get { return true; }
+        }
+
+        public abstract string PreviousEntryActionName { get; }
+        public abstract string NextEntryActionName { get; }
+        public abstract string SelectedEntryActionName { get; }
+        public abstract string MenuCancelActionName { get; }
+
+        public void SetDescriptionArea(Rectangle position, Color boxColor, Color textColor, float scale)
+        {
+            SetDescriptionArea(position, boxColor, textColor, new Point(0, 0), scale);
+        }
+
+        public void SetDescriptionArea(Rectangle position, Color boxColor, Color textColor, Point padding, float scale)
+        {
+            DescriptionPosition = position;
+            _descriptionTexture = new Texture2D(ScreenManager.GraphicsDevice, 1, 1);
+            _descriptionTexture.SetData(new[] { boxColor });
+            _descriptionBoxColor = boxColor;
+            _descriptionColor = textColor;
+            DescriptionPadding = padding;
+            _descriptionScale = scale;
+        }
+
+        public void EnableMouse(Texture2D texture)
+        {
+            if (texture != null)
+            {
+                MouseTexture = texture;
+                _mouseVisible = true;
+            }
+        }
+
+        public void ShowMouse()
+        {
+            _mouseVisible = true;
+        }
+
+        public void HideMouse()
+        {
+            _mouseVisible = false;
         }
 
         public virtual void MenuCancel()
@@ -33,25 +136,139 @@ namespace GameEngine
             }
         }
 
+        public override void HandleInput()
+        {
+            if (InputMap.NewActionPress(PreviousEntryActionName))
+            {
+                _menuEntries[_selectedEntry].Normal();
+                _selectedEntry--;
+                if (_selectedEntry < 0)
+                {
+                    _selectedEntry = _menuEntries.Count - 1;
+                }
+                _menuEntries[_selectedEntry].Highlight();
+            }
+
+            if (InputMap.NewActionPress(NextEntryActionName))
+            {
+                _menuEntries[_selectedEntry].Normal();
+                _selectedEntry++;
+                if (_selectedEntry >= _menuEntries.Count)
+                {
+                    _selectedEntry = 0;
+                }
+                _menuEntries[_selectedEntry].Highlight();
+            }
+
+            if (MouseTexture != null && _mouseVisible)
+            {
+                for (int i = _menuEntries.Count - 1; i >= 0; i--)
+                {
+                    if (_menuEntries[i].BoundingRectangle.Intersects(MouseBounds))
+                    {
+                        _menuEntries[_selectedEntry].Normal();
+                        _selectedEntry = i;
+                        _menuEntries[_selectedEntry].Highlight();
+                    }
+                }
+            }
+
+            if (InputMap.NewActionPress(SelectedEntryActionName))
+            {
+                _menuEntries[_selectedEntry].Select();
+            }
+
+            if (InputMap.NewActionPress(MenuCancelActionName))
+            {
+                MenuCancel();
+            }
+        }
+
+        public abstract override void InitializeScreen();
+        public abstract override void LoadContent();
+
+        protected override void UpdateScreen(GameTime gameTime)
+        {
+            if (_menuEntries.Count == 0)
+            {
+                return;
+            }
+
+            if (_menuEntries[_selectedEntry].State != MenuEntry.EntryState.Highlight)
+            {
+                _menuEntries[_selectedEntry].Highlight();
+            }
+
+            for (int i = 0; i < _menuEntries.Count; i++)
+            {
+                _menuEntries[i].UpdateEntry(gameTime);
+                _menuEntries[_selectedEntry].AnimateHighlighted(gameTime);
+            }
+
+            if (MouseTexture != null)
+            {
+                Vector2 mousePos = InputMap.GetMousePosition();
+                MouseBounds = new Rectangle((int)mousePos.X, (int)mousePos.Y, MouseTexture.Width, MouseTexture.Height);
+            }
+        }
+
         protected override void DrawScreen(GameTime gameTime)
         {
             var spriteBatch = ScreenManager.SpriteBatch;
+            
             if (BackgroundTexture != null)
             {
                 spriteBatch.Draw(BackgroundTexture, BackgroundPosition, Color.White);
             }
+
+            if (TitleTexture != null)
+            {
+                spriteBatch.Draw(TitleTexture, TitlePosition, Color.White * _titleOpacity);
+            }
+
+            for (int i = 0; i < _menuEntries.Count; i++)
+            {
+                _menuEntries[i].Draw(gameTime, i == _selectedEntry);
+            }
+
+            DrawDescriptionArea(spriteBatch, gameTime);
+            DrawMouse(spriteBatch, gameTime);
         }
 
         private void MenuScreen_Removing(object sender, EventArgs e)
         {
+            _menuEntries.Clear();
             if (Parent != null && Parent.State == ScreenState.Frozen)
             {
                 Parent.ActivateScreen();
-                ////var ms = Parent as MenuScreen;
-                ////if (ms != null && ms.MouseTexture != null)
-                ////{
-                ////    ms.ShowMouse();
-                ////}
+                var ms = Parent as MenuScreen;
+                if (ms != null && ms.MouseTexture != null)
+                {
+                    ms.ShowMouse();
+                }
+            }
+        }
+
+        private void DrawDescriptionArea(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            String s = _menuEntries[_selectedEntry].EntryDescription;
+            if (!String.IsNullOrEmpty(s) && _descriptionTexture != null)
+            {
+                spriteBatch.Draw(_descriptionTexture, DescriptionPosition, _descriptionBoxColor);
+                _menuEntries[_selectedEntry].DrawDescription(
+                    gameTime,
+                    DescriptionPosition,
+                    DescriptionPadding,
+                    _descriptionColor,
+                    _descriptionScale);
+            }
+        }
+
+        private void DrawMouse(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            if (MouseTexture != null && _mouseVisible)
+            {
+                spriteBatch.Draw(MouseTexture, InputMap.GetMousePosition(), Color.White);
             }
         }
     }
